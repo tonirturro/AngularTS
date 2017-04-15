e8e4a7ad010e7520d730a6237248cd05311cc158
Antoni Garcia <Antoni Garcia>
2017-04-15 11:49:40 +0200

    Included server side and project structure

--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,8 @@ const express = require ('express');
 const app = express();
 const server = http.createServer(app);
 
-const view_path = "/View"
+const view_path = "/View";
+const server_path = "/Server";
 
 // Main page and includes 
 app.get("/", function(err, res) {
@@ -42,7 +43,7 @@ app.get(app_path, function(err, res) {
 // The simulated 
 const model_path = "/data.json";
 app.get(model_path, function(err, res) {
-    res.sendFile(__dirname + model_path);
+    res.sendFile(__dirname + server_path + model_path);
 });
 
 // App start

228ef4705686ed60a8b3b987472208d8361200c8
Antoni Garcia <Antoni Garcia>
2017-04-17 13:44:37 +0200

    Basic REST implementation

--- Src/Server/App.ts
+++ Src/Server/App.ts
@@ -1,8 +1,16 @@
 
 // Define and export a singletpn for the application.
 import * as express from 'express';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
 
 class App {
+    // Routes
+
+    private readonly rest_path = "/REST";
+    private readonly view_path = "/View";
+    private readonly lib_path = "/Lib";
+
     // Host express
     public express: express.Application;
 
@@ -10,42 +18,42 @@ class App {
     constructor() {
         this.express = express();
         this.setStaticServer();
+        this.setRESTServer();
+        this.routes();
     }
 
     // Configure the files that will be served statically.
     private setStaticServer(): void {
-        const view_path = "/View";
-        const lib_path = "/Lib";
-
-        // Main page and includes 
+        // Main page and includes
+        const index_path = this.view_path + "/index.htm";
         this.express.get("/", function(err, res) {
-            res.sendFile(__dirname + view_path + "/index.htm");
+            res.sendFile(__dirname + index_path);
         });
 
-        const form_path =  view_path + "/users_form.htm";
+        const form_path = this.view_path + "/users_form.htm";
         this.express.get(form_path, function(err, res) {
             res.sendFile(__dirname + form_path);
         })
 
-        const grid_path =  view_path + "/users_grid.htm";
+        const grid_path =this.view_path + "/users_grid.htm";
         this.express.get(grid_path, function(err,res) {
             res.sendFile(__dirname + grid_path);
         });
 
         // Bootstrap
-        const bootstrap_path = lib_path + "/bootstrap.css";
+        const bootstrap_path = this.lib_path + "/bootstrap.css";
         this.express.get(bootstrap_path, function(err, res) {
             res.sendFile(__dirname + bootstrap_path);
         });
 
         // Angular
-        const angular_path = lib_path + "/angular.js";
+        const angular_path = this.lib_path + "/angular.js";
         this.express.get(angular_path, function(err, res) {
             res.sendFile(__dirname + angular_path);
         });
 
         // Application
-        const app_path =  view_path + "/scripts/main.js";
+        const app_path =this.view_path + "/scripts/main.js";
         this.express.get(app_path, function(err, res) {
             res.sendFile(__dirname + app_path);
         });
@@ -55,6 +63,26 @@ class App {
         this.express.get(model_path, function(err, res) {
             res.sendFile(__dirname + model_path);
         });
+    }
+
+    // Configure Express middleware for REST API
+    private setRESTServer() {
+        this.express.use(logger('dev'));
+        this.express.use(bodyParser.json());
+    }
+
+    // Configure routes
+    private routes(): void
+    {
+        let router = express.Router();
+
+        router.get("/", (req, res) => {
+            res.json({
+                message: 'Hello World'
+            });
+        });
+
+        this.express.use(this.rest_path, router);
     }
 }
 

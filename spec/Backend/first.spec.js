b040893792c6a68181f454080422c43e6ec49f9b
Antoni Garcia <tonirturro@outlook.es>
2017-04-24 22:17:59 +0200

    Jasmine REST TEST

--- spec/Backend/first.spec.js
+++ spec/Backend/first.spec.js
@@ -1,6 +1,19 @@
-var value = 0;
-describe("First Spec", function() {
-    it("First Test", function() {
-        expect(value).toEqual(0);
-    })
+var request = require("request");
+var base_url = "http://localhost:3000/";
+
+describe("Test Server", function() {
+    it("Get REST API Base Code", function(done) {
+        request.get(base_url + "REST", function(error, response, body) {
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+
+    it("Get REST API Base Answer", function(done) {
+        request.get(base_url + "REST", function(error, response, body) {
+            var data = JSON.parse(body);
+            expect(data.message).toBe("Hello World");
+            done();
+        });
+    });
 });

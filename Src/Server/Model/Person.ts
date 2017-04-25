d6b1d0557d7813107f11a0c815d0a6808fd6429a
Antoni Garcia <tonirturro@outlook.es>
2017-04-25 12:03:46 +0200

    Person entity and needed typings.

---
+++ Src/Server/Model/Person.ts
@@ -0,0 +1,16 @@
+﻿//
+// Defines the Person entity
+// 
+
+class Person {
+    id: number;
+    givenName: string;
+    famillyName: string;
+
+    // Constructor
+    constructor(id: number, givenName: string, famillyName: string) {
+        this.id = id;
+        this.givenName = givenName;
+        this.famillyName = famillyName;
+    }
+}

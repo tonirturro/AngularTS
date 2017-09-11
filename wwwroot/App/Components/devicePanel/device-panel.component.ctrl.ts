f6700138b7e572086c0ac5ef806a2798a7c9cd8d
Antoni Garcia <tonirturro@outlook.es>
2017-09-11 22:53:05 +0200

    Device selection

--- wwwroot/App/Components/devicePanel/device-panel.component.ctrl.ts
+++ wwwroot/App/Components/devicePanel/device-panel.component.ctrl.ts
@@ -4,12 +4,31 @@ import { DataService } from "../../Services/DataService";
 import { ModelUpdate } from  "../../Model/ModelEvents";
 
 /**
+ * Data to be displayed for a device
+ */
+class DeviceDisplay {
+    id: number;
+    name: string;
+    class: string;
+
+    // Constructor
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+        this.class = "";
+    }
+}
+
+/**
  * handles the bindings inside the component
  */
 export class DevicePanelController {
 
     // The devices to be displayed at the panel
-    private devices_: Device[];
+    private devices_: DeviceDisplay[];
+
+    // The selected device
+    private selectedDevice_:number;
 
     /**
      * Initializes a new instance of the DevicePanelController class.
@@ -21,24 +40,17 @@ export class DevicePanelController {
         private logService: angular.ILogService,
         private rootScopeService: angular.IRootScopeService,
         private dataService: DataService) {
-            this.devices_ = [];
-            this.dataService.getDevices().then(devices => {
-                this.devices_ = devices;
-                logService.log(`Loaded ${this.devices_.length} devices`);
-            })
-            .catch(reason => {
-                logService.error("Failed to load devices");
-            });
+            this.loadDevices();
+
+            if (this.devices_.length > 0) {
+                this.selectedDevice_ = this.devices_[0].id;
+            }
+
+            this.displaySelection();
 
             // Capture model events
             this.rootScopeService.$on(ModelUpdate.Devices.toString(), () => {
-                this.dataService.getDevices().then(devices => {
-                    this.devices_ = devices;
-                    logService.log(`Loaded ${this.devices_.length} devices`);
-                })
-                .catch(reason => {
-                    logService.error("Failed to load devices");
-                });
+                this.loadDevices();
             });
         }
 
@@ -56,13 +68,48 @@ export class DevicePanelController {
     deleteDevice(deviceId: number):void {
         this.dataService.deleteDevice(deviceId).then(sucess => {
             if (sucess) {
-                this.logService.log(`Deleted device id ${deviceId}`);
-                this.dataService.getDevices().then(devices => {
-                    this.devices_ = devices;
-                    this.logService.log(`Loaded ${this.devices_.length} devices`);
-                })               
+                this.loadDevices();              
             } else {
                 this.logService.log(`Failed to delete device id ${deviceId}`);
+            }
+        });
+    }
+
+    /**
+     * Selects a device
+     * @param deviceId the id for the selected device
+     */
+    selectDevice(deviceId: number):void {
+        this.selectedDevice_ = deviceId;
+        this.displaySelection();
+    }
+
+    /**
+     * Load the existing devices
+     */
+    private loadDevices():void {
+        this.devices_ = [];
+        this.dataService.getDevices().then(devices => {
+            this.devices_ = []
+            devices.forEach(device => {
+                this.devices.push(new DeviceDisplay(device.id, device.name));
+            });
+        })
+        .catch(reason => {
+            this.logService.error("Failed to load devices");
+        });        
+    }
+
+    /**
+     * Displays the visual selection
+     */
+    private displaySelection(): void {
+        // Set selected style
+        this.devices_.forEach(device => {
+            if (device.id === this.selectedDevice_) {
+                device.class = "item-selected";
+            } else {
+                device.class = "";
             }
         });
     }

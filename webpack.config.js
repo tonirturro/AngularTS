e8915e78e5339dd3fefa9841baae806b9eeea007
Antonio Garcia <tonirturro@outlook.es>
2017-10-02 11:41:13 -0700

    TS Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 
 module.exports = {
-    entry: './wwwroot/App/Boot.js',
+    entry: './wwwroot/App/Boot.ts',
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist/bundle')
+    },
+    resolve: {
+        extensions: ['.ts' ]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                exclude: path.resolve(__dirname, 'node_modules'),
+                loader: 'ts-loader?configFileName=tsconfig.webpack.json'
+            }
+        ]
     }
 };

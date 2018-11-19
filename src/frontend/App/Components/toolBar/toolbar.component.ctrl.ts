4953b03c88a504bb96f4c827a9a27735a488b008
Antonio Garcia <tonirturro@outlook.es>
2018-11-19 19:02:51 +0100

    Add new buttons to toolbar

--- src/frontend/App/Components/toolBar/toolbar.component.ctrl.ts
+++ src/frontend/App/Components/toolBar/toolbar.component.ctrl.ts
@@ -1,16 +1,28 @@
+import { IWindowService } from "angular";
+
 /**
  * Handles the bindings inside the component
  */
 export class ToolBarController {
+    public static $inject = [ "$window" ];
     /**
      * Bindings
      */
     public onAddDevice: () => void;
+
+    constructor(private $window: IWindowService) {}
 
     /**
      * Report add action
      */
     public addDevice(): void {
         this.onAddDevice();
+    }
+
+    /**
+     * Close main window
+     */
+    public close() {
+        this.$window.close();
     }
 }
